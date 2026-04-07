e3497fd8c3b318f0361663501dc029e9e18f2a56
caioonofre <protectcaio@gmail.com>
2026-04-07 13:41:34 -0300

    Header finalizado com css

---
+++ src/Components/Header.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
+import Dogs from '/src/assets/dogs.svg?react';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to="/login" aria-label="Dogs - Login">
+          Login / Criar
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
