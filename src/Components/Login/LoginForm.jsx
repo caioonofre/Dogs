720a4b89b756ef8cc7feaf72d82c7a2640465f3f
caioonofre <protectcaio@gmail.com>
2026-04-15 15:54:14 -0300

    UseForm mais complementos

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,19 +1,19 @@
-import { func } from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../Forms/Button';
+import UseForm from '../../Hooks/UseForm';
 import Input from '../Forms/Input';
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const username = UseForm();
+  const password = UseForm();
 
   function handleSubmit(event) {
     event.preventDefault();
     fetch('https://www.dogsapi.origamid.dev/json/jwt-auth/v1/token', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({}),
     })
       .then((response) => {
         console.log(response);
@@ -28,8 +28,8 @@ const LoginForm = () => {
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to="/Login/criar">Cadastro</Link>
